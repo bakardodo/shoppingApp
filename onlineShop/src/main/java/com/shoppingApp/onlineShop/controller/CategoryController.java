24ca153e2c6121301550060966a6b3c497cc79cc
Stoyan Bakardzhiev <bakardjievstoqn@gmail.com>
2023-08-23 18:03:46 +0300

    Add Category update api and ApiResponse class

--- onlineShop/src/main/java/com/shoppingApp/onlineShop/controller/CategoryController.java
+++ onlineShop/src/main/java/com/shoppingApp/onlineShop/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.shoppingApp.onlineShop.controller;
 
+import com.shoppingApp.onlineShop.common.ApiResponse;
 import com.shoppingApp.onlineShop.model.Category;
 import com.shoppingApp.onlineShop.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,18 +15,27 @@ import java.util.List;
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
-    }
+
     @PostMapping("/create")
-    public String createCategory(@RequestBody Category category){
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category){
         categoryService.createCategory(category);
-        return "Success";
+        return new ResponseEntity<>(new ApiResponse(true, "create a new category"),
+                HttpStatus.CREATED);
     }
     @GetMapping("/list")
     public List<Category> listCategory(){
         return categoryService.listCategory();
 
+    }
+
+    @PostMapping("/update/{categoryId}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryId") int categoryId, @RequestBody Category category){
+        System.out.println("categoryId " + categoryId);
+        if(!categoryService.findById(categoryId)){
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category does not exist"), HttpStatus.NOT_FOUND);
+
+        }
+        categoryService.editCategory(categoryId, category);
+        return new ResponseEntity<ApiResponse>(new ApiResponse(true, "category has been updated"), HttpStatus.OK);
     }
 }
--- onlineShop/src/main/java/com/shoppingApp/onlineShop/repository/CategoryRepository.java
+++ onlineShop/src/main/java/com/shoppingApp/onlineShop/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import com.shoppingApp.onlineShop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+
 }
